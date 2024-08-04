11f0251e81ff328537812a6a436bfb0a89cba904
Engmohamed12 <149935063+Engmohamed12@users.noreply.github.com>
2024-08-04 14:17:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// let products = [
+//     {
+//         id:1,
+//         image:"task 1.png",
+//     },
+//     {
+//         id:2,
+//         image:"task 2.png",
+//     },
+//     {
+//         id:3,
+//         image:"task 3.png",
+//     },
+//     {
+//         id:4,
+//         image:"task 4.png",
+//     },
+//     {
+//         id:5,
+//         image:"task 5.png",
+//     },
+// ]
+
+// function initApp() {
+//     products.forEach((value) => {
+//         let newDiv = document.createElement("div");
+//         newDiv.classList.add("slide");
+//         newDiv.innerHTML =`
+//         <img src=${value.image}>
+//         `;
+//         task.appendChild(newDiv);
+//     });
+// }
+
+// initApp();
